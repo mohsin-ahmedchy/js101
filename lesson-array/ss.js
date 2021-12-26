66cd8609f9a6df7b68faed375b4f8ce9b82b8c88
Mohsin Ahmed <mohsinahmednuhat@gmail.com>
2021-12-26 23:32:49 +0100

    ss

---
+++ lesson-array/ss.js
@@ -0,0 +1,18 @@
+
+
+//slice():
+const arr = ['Mohsin','Abir','Xaber','Eric','Agi','Moin'];
+console.log(arr.slice(1));
+console.log(arr.slice(1,3));//slice(startIndex,andIndex)
+console.log(arr.slice(-1));
+console.log(arr.slice(1,-2));
+
+
+//splice():
+const arr_1 = ['Mohsin','Abir','Xaber','Eric','Agi','Moin'];
+console.log(arr_1.splice(-1));
+console.log(arr_1);
+console.log(arr_1.splice(1,2));//splice(startIndex,number of string delete from the array.)
+console.log(arr_1);
+
+
