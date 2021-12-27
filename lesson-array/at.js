5e780ba20ce04885a6ddb61c7cb40c53f4d2177d
Mohsin Ahmed <mohsinahmednuhat@gmail.com>
2021-12-27 23:17:13 +0100

    at

---
+++ lesson-array/at.js
@@ -0,0 +1,13 @@
+//at():
+
+const arr = [1,2,3,4,7,8,9];
+
+//solution-1:
+console.log(arr[0]);
+console.log(arr[arr.length-1]);
+
+
+
+//solution-2:
+console.log(arr.at(1));
+console.log('jonas'.at(-1));
