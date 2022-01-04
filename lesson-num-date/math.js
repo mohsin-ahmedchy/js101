eddaebb06d4a912440d2d6ea91fda1bc2a0b87fb
Mohsin Ahmed <mohsinahmednuhat@gmail.com>
2022-01-04 11:14:59 +0100

    math.js

---
+++ lesson-num-date/math.js
@@ -0,0 +1,16 @@
+console.log(Math.sqrt(36));
+console.log(25**(1/2));
+console.log(27**(1/3));
+
+
+console.log(Math.round(3.45));
+console.log(Math.round(3.95));
+console.log((3.456).toFixed(1));//value is a string
+console.log(+(3.456).toFixed(1));//value is a number
+console.log(Math.ceil(3.45));
+console.log(Math.floor(3.45));
+console.log(Math.random());
+console.log(Math.trunc(Math.random()*5));
+console.log(Math.max(34,5,76,8));
+console.log(Math.min(34,5,76,8));
+console.log(Math.PI);
