4f5282f4c8542a21aede9f61024a4bc216ebe297
Mohsin Ahmed <mohsinahmednuhat@gmail.com>
2022-02-01 23:31:13 +0000

    function-1

---
+++ Function/func_1.js
@@ -0,0 +1,24 @@
+'use strict'
+
+// Default Parameter:
+const bookings = [];
+
+const createBooking = function(flightName,passengers=23,seats=100){
+
+
+    const booking = {
+        flightName,
+        passengers,
+        seats
+    };
+
+    console.log(booking);
+
+    bookings.push(booking);
+}
+
+createBooking('LH101',34,100);
+createBooking('LH101',3);
+
+console.log(bookings);
+console.log(bookings[0]);
