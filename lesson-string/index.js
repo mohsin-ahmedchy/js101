5f97af0cfe4ff77d2983c29362067a392bcbaa75
Mohsin Ahmed <mohsinahmednuhat@gmail.com>
2022-02-09 19:51:04 +0000

    updated

--- lesson-string/index.js
+++ lesson-string/index.js
@@ -3,3 +3,22 @@
 const data = 'Mohsin';
 
 console.log(data.charAt(2));
+
+
+//switch:
+
+let word = prompt('enter the letter:');
+
+
+switch(word){
+      case 'a':
+      case 'e':
+      case 'i':
+      case 'o':
+      case 'u':
+              console.log('vowel');
+              break;
+ 
+      default:
+              console.log('consonant');
+}
